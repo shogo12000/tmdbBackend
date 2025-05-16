b2ed5edaabcea3872b17cc8c28f45fc5f88ed6a6
sidney <shogo12000@hotmail.com>
2025-05-15 23:46:21 -0700

    second commit

--- routes/auth.js
+++ routes/auth.js
@@ -51,7 +51,7 @@ router.post('/login', async (req, res) => {
 
         res.cookie('session_token', token, {
             httpOnly: true,
-            secure: false,
+            secure: true,
             sameSite: 'Strict',
             maxAge: 60 * 60 * 1000
         });
