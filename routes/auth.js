4aeafa829fb47de3a2d980d87b0789fdc8988d56
sidney <shogo12000@hotmail.com>
2025-05-15 21:59:44 -0700

    second commit

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,17 @@ const User = require('../models/Users');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+const { OAuth2Client } = require('google-auth-library');
+
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '475376767328-soku8p9dl739un1ggshsnfijo5bjcc3m.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
 
 router.use((req, res, next) => {
     //fazer a validacao do token aqui 
     console.log("PASSOU ROUTER USER")
     next()
 })
+
 
 router.get('/', (req, res) => {
     res.send('Backend funcionandoX!');
@@ -31,26 +36,92 @@ router.post('/register', async (req, res) => {
     res.status(201).json({ message: 'usuario criado com sucesso' });
 })
 
+
 router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
+ 
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: 'Usuário não encontrado' });
 
-    const user = await User.findOne({ email });
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) return res.status(400).json({ message: 'Senha incorreta' });
 
-    if (!user) return res.status(400).json({ message: 'Usuário não encontrado' });
+        const token = jwt.sign({ email: email }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) return res.status(400).json({ message: 'Senha incorreta' });
+        res.cookie('session_token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'Strict',
+            maxAge: 60 * 60 * 1000
+        });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ message: 'Login bem-sucedido' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Erro no servidor' });
+    }
+});
 
-    res.cookie('token', token, {
+
+router.post('/logout', (req, res) => {
+    res.clearCookie('session_token', {
         httpOnly: true,
-        secure: true,              // só funciona com HTTPS (obrigatório em produção)
-        sameSite: 'Strict',        // ou 'Lax'/'None' dependendo do seu frontend
-        maxAge: 60 * 60 * 1000     // 1 hora
+        secure: true,
+        sameSite: 'Strict'
     });
 
-    res.json({ message: "funcionando"})
+    res.status(200).json({ message: 'Logout realizado com sucesso' });
 });
+
+router.post('/google', async (req, res) => {
+    console.log("CHEGOU GOOGLE")
+    const { credential } = req.body; // o token JWT do Google (ID Token)
+
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: credential,
+            audience: CLIENT_ID,
+        });
+
+        const payload = ticket.getPayload(); // Dados do usuário
+
+        const { email, name, picture } = payload;
+
+        const token = jwt.sign(
+            { email: email },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        // Aqui você pode salvar no banco de dados, criar token JWT, etc
+        // Exemplo: salvar em cookie
+        res.cookie('session_token', token, {
+            httpOnly: true,
+            secure: true, // true em produção com HTTPS
+            sameSite: 'Lax',
+        });
+
+        res.json({ message: 'Usuário autenticado', user: { email, name, picture } });
+
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({ error: 'Token inválido' });
+    }
+});
+
+router.get('/me', (req, res) => {
+    const token = req.cookies.session_token;
+
+    if (!token) return res.status(401).json({ message: 'Não autenticado' });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        res.json({ user: decoded }); // { email, name }
+    } catch (err) {
+        res.status(401).json({ message: 'Token inválido' });
+    }
+
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,22 +2,23 @@
 const express = require('express'); 
 const mongoose = require('mongoose');
 const cors = require('cors');
-const authRoutes = require('./routes/auth');
+const authRoutes = require('./routes/auth'); 
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use('/api/auth', authRoutes);
+app.use(cookieParser());
  
 app.use(cors({
-  origin: '*', // coloque a origem do seu frontend aqui
+  origin: 'http://localhost:5173', // coloque a origem do seu frontend aqui
   credentials: true // se  for usar cookies ou auth com headers
 }));
+ 
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 4000;
- 
-
+  
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('✅ MongoDB conectado');
   app.listen(PORT, () => {
