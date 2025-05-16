39c0d7748bed774586f0fc19cdbd9ee0e3e2bd7d
sidney <shogo12000@hotmail.com>
2025-05-15 23:13:00 -0700

    second commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://front-end-login-two.vercel.app/'
+  'https://front-end-login-two.vercel.app'
 ];
 
 app.use(cors({
