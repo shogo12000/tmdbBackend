1d8ed908a68bae57a597373f278b409291d1a1ac
sidney <shogo12000@hotmail.com>
2025-05-15 23:01:12 -0700

    second commit

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
 //https://dashboard.render.com/web/srv-d0i5otmmcj7s739nsj10/events
-const express = require('express'); 
+const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const authRoutes = require('./routes/auth'); 
+const authRoutes = require('./routes/auth');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
- 
+
+
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://front-end-login-klwyavxyc-shogo12000s-projects.vercel.app'
+];
+
 app.use(cors({
-  origin: '*', // coloque a origem do seu frontend aqui x
+  //origin: '*', // coloque a origem do seu frontend aqui x
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true // se  for usar cookies ou auth com headers
 }));
- 
+
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 4000;
-  
+
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('✅ MongoDB conectado');
   app.listen(PORT, () => {
